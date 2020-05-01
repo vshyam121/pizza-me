720c28c036b91353f612d0ddd2c048079b09ab8f
Shya <shya@Shyams-MacBook-Pro.local>
2020-05-01 01:32:24 -0700

    UI changes to cart and adding totals of all items

--- src/containers/Cart/Cart.js
+++ src/containers/Cart/Cart.js
@@ -6,7 +6,6 @@ import { changeItemQuantity, removeItem } from "../../store/cart/cartActions";
 import CartItem from "../../components/CartItem/CartItem";
 
 const Cart = props => {
-
   const handleEditItem = (item, itemId) => {
     props.initializePizzaBuilder(item, itemId);
   };
@@ -15,12 +14,29 @@ const Cart = props => {
     props.changeItemQuantity(event.target.value, itemId);
   };
 
-  const handleRemoveItem = (itemId) => {
+  const handleRemoveItem = itemId => {
     props.removeItem(itemId);
-  }
+  };
+
+  const calculateSubTotal = () => {
+    let subTotal = 0;
+    Object.values(props.items).forEach(item => {
+      subTotal += item.price * item.quantity;
+    });
+    return subTotal.toFixed(2);
+  };
+
+  const calculateTax = subTotal => {
+    return (subTotal * 0.1).toFixed(2);
+  };
+
+  const subTotal = calculateSubTotal();
+  const tax = calculateTax(subTotal);
+  const total = (+subTotal + +tax).toFixed(2);
 
   return (
     <div className="cart">
+      <h1 className="cart__title">My Cart</h1>
       {Object.keys(props.items).map(itemId => {
         const item = props.items[itemId];
         return (
@@ -33,6 +49,13 @@ const Cart = props => {
           />
         );
       })}
+      <div className="cart__totals">
+        <div className="cart__total-line-item">
+          <span>Subtotal:</span> <span>${subTotal}</span>
+        </div>
+        <div className="cart__total-line-item"><span>Tax:</span> <span>${tax}</span></div>
+        <div className="cart__total-line-item"><span>Total:</span> <span>${total}</span></div>
+      </div>
     </div>
   );
 };
