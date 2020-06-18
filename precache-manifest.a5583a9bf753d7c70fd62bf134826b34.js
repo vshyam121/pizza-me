b2649acf65caf83089ecbd1c7951b42e933ecba1
Shya <shya@Shyams-MacBook-Pro.local>
2020-06-17 17:49:15 -0700

    Updates

---
+++ precache-manifest.a5583a9bf753d7c70fd62bf134826b34.js
@@ -0,0 +1,342 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a09ac11c932bdcd36fd1dc132c48c193",
+    "url": "/pizza-me/index.html"
+  },
+  {
+    "revision": "8b78211362923067cb0a",
+    "url": "/pizza-me/static/css/main.3236a0d4.chunk.css"
+  },
+  {
+    "revision": "994bf79ed5e2f05e5dff",
+    "url": "/pizza-me/static/js/2.67cdfcd7.chunk.js"
+  },
+  {
+    "revision": "ecd6ed2ecafff99a00eb11a8439eff3a",
+    "url": "/pizza-me/static/js/2.67cdfcd7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b78211362923067cb0a",
+    "url": "/pizza-me/static/js/main.398975aa.chunk.js"
+  },
+  {
+    "revision": "a4cf6b570e21ec0e2825",
+    "url": "/pizza-me/static/js/runtime-main.5ccde37c.js"
+  },
+  {
+    "revision": "e343a2cbf353bf5377f8a298c5832f7a",
+    "url": "/pizza-me/static/media/alarm-clock.e343a2cb.png"
+  },
+  {
+    "revision": "a605849df9785158a823da7d0de3852f",
+    "url": "/pizza-me/static/media/cheese_mozz_ML.a605849d.webp"
+  },
+  {
+    "revision": "22e458d005378a09c6b155630e46fd61",
+    "url": "/pizza-me/static/media/crust_ht_GarlicButteryBlend.22e458d0.webp"
+  },
+  {
+    "revision": "08aa34b3aa01304a8edff9709425209e",
+    "url": "/pizza-me/static/media/crust_ht_NoFinish.08aa34b3.webp"
+  },
+  {
+    "revision": "89f0f86fe0a8504777226e05710932ec",
+    "url": "/pizza-me/static/media/crust_ht_ToastedParmesan.89f0f86f.webp"
+  },
+  {
+    "revision": "904ed9e38d85112f17bc6e8e60afabfe",
+    "url": "/pizza-me/static/media/crust_pan_GarlicButteryBlend.904ed9e3.webp"
+  },
+  {
+    "revision": "ee96838d5a736069aa347d788f13ee5a",
+    "url": "/pizza-me/static/media/crust_pan_NoFinish.ee96838d.webp"
+  },
+  {
+    "revision": "8ad52dc82701e2ebde137b5b3f228351",
+    "url": "/pizza-me/static/media/crust_pan_ToastedParmesan.8ad52dc8.webp"
+  },
+  {
+    "revision": "bf3745a17a73f1ab760f5a4f0d8eab58",
+    "url": "/pizza-me/static/media/crust_thin_GarlicButteryBlend.bf3745a1.webp"
+  },
+  {
+    "revision": "adbed319bfbcd77534054fa4589a672b",
+    "url": "/pizza-me/static/media/crust_thin_NoFinish.adbed319.webp"
+  },
+  {
+    "revision": "f38d858f7cb22fe5d48065697a5b415d",
+    "url": "/pizza-me/static/media/crust_thin_ToastedParmesan.f38d858f.webp"
+  },
+  {
+    "revision": "3557515d7b1807be24d7825ba13f185f",
+    "url": "/pizza-me/static/media/icon_crust_ht.3557515d.webp"
+  },
+  {
+    "revision": "a7bcbde8a54a9a92b382549aa6fecd21",
+    "url": "/pizza-me/static/media/icon_crust_pan.a7bcbde8.webp"
+  },
+  {
+    "revision": "0809ee75fe124e1c5e4453dd0f7ffc22",
+    "url": "/pizza-me/static/media/icon_crust_thin.0809ee75.webp"
+  },
+  {
+    "revision": "69a08ee0f1949203bb47f6eebb00a8cd",
+    "url": "/pizza-me/static/media/icon_finisher_GarlicButteryBlend_whole_2017.69a08ee0.webp"
+  },
+  {
+    "revision": "76367cf47fe4ed8aba075761eeb04664",
+    "url": "/pizza-me/static/media/icon_finisher_ToastedParm_whole_2017.76367cf4.webp"
+  },
+  {
+    "revision": "0548e95533779d58bf31187a45c27995",
+    "url": "/pizza-me/static/media/icon_finisher_none_whole_2017.0548e955.webp"
+  },
+  {
+    "revision": "887f3ff7029cac4e63c5b274aa3d51c9",
+    "url": "/pizza-me/static/media/icon_sauce_BBQ.887f3ff7.webp"
+  },
+  {
+    "revision": "c340f1717333dc8d2733cb175d81a5d8",
+    "url": "/pizza-me/static/media/icon_sauce_Buffalo.c340f171.webp"
+  },
+  {
+    "revision": "633be6596ca645b01d23265e58b7cda8",
+    "url": "/pizza-me/static/media/icon_sauce_CreamyGarlicParm.633be659.webp"
+  },
+  {
+    "revision": "c98802dd28ad9a83a0775dcca64142d9",
+    "url": "/pizza-me/static/media/icon_sauce_Marinara.c98802dd.webp"
+  },
+  {
+    "revision": "e2a24d1f4788e857565a81e8575045b6",
+    "url": "/pizza-me/static/media/icon_topping_Bacon.e2a24d1f.webp"
+  },
+  {
+    "revision": "109620c9c47a3bdf11a5e4d733924b6e",
+    "url": "/pizza-me/static/media/icon_topping_BananaPepper.109620c9.webp"
+  },
+  {
+    "revision": "2c1074f4730ee6a42aa4feb8481c2cd7",
+    "url": "/pizza-me/static/media/icon_topping_Beef.2c1074f4.webp"
+  },
+  {
+    "revision": "643cd30bd9e6823de20cca626fbc6dff",
+    "url": "/pizza-me/static/media/icon_topping_Chicken.643cd30b.webp"
+  },
+  {
+    "revision": "70360e2274fe2e4af645372f86fe03c6",
+    "url": "/pizza-me/static/media/icon_topping_GreenBellPepper.70360e22.webp"
+  },
+  {
+    "revision": "6704f9944f45d8003e346efef63e784f",
+    "url": "/pizza-me/static/media/icon_topping_Ham.6704f994.webp"
+  },
+  {
+    "revision": "54809d4cfeb6642521f4efd09486c8e3",
+    "url": "/pizza-me/static/media/icon_topping_ItalianSausage.54809d4c.webp"
+  },
+  {
+    "revision": "c0c19e4a643fa93deb1464e662650393",
+    "url": "/pizza-me/static/media/icon_topping_Jalapeno.c0c19e4a.webp"
+  },
+  {
+    "revision": "385ba86f087f7152ec4bcaee676f9cda",
+    "url": "/pizza-me/static/media/icon_topping_Mushroom.385ba86f.webp"
+  },
+  {
+    "revision": "437ec9fc67d5ed1a07fa1f59a121fe38",
+    "url": "/pizza-me/static/media/icon_topping_Olive_black.437ec9fc.webp"
+  },
+  {
+    "revision": "ee0fc2ea800f1af1624c730fdcd89111",
+    "url": "/pizza-me/static/media/icon_topping_Onion_Red.ee0fc2ea.webp"
+  },
+  {
+    "revision": "be37385e5c9db0a96967d938cc02b9f0",
+    "url": "/pizza-me/static/media/icon_topping_Pepperoni.be37385e.webp"
+  },
+  {
+    "revision": "f75e82a754aeab769e325f7cf1759e8a",
+    "url": "/pizza-me/static/media/icon_topping_Pineapple.f75e82a7.webp"
+  },
+  {
+    "revision": "c211fb9e641b8d3b539ac7d55b3cf684",
+    "url": "/pizza-me/static/media/icon_topping_Pork.c211fb9e.webp"
+  },
+  {
+    "revision": "ed63590c08f8a64d8830baefa0d761b4",
+    "url": "/pizza-me/static/media/icon_topping_Spinach_fresh.ed63590c.webp"
+  },
+  {
+    "revision": "9f731660704d627acf09a727e5b95aaa",
+    "url": "/pizza-me/static/media/icon_topping_Tomato.9f731660.webp"
+  },
+  {
+    "revision": "6231951ec772e48644b00f87041841c3",
+    "url": "/pizza-me/static/media/pizza_Backyard_BBQ_Chicken.6231951e.webp"
+  },
+  {
+    "revision": "7a129682c6d8bdc1e10dec87ec088427",
+    "url": "/pizza-me/static/media/pizza_Buffalo_Chicken.7a129682.webp"
+  },
+  {
+    "revision": "6e55a5fbd8e49a56ce5fb6706c3438f1",
+    "url": "/pizza-me/static/media/pizza_Chicken_Bacon_Parmesan.6e55a5fb.webp"
+  },
+  {
+    "revision": "e782cdd1f2e587ca9a2237603c81b91b",
+    "url": "/pizza-me/static/media/pizza_Hawaiian_Chicken.e782cdd1.webp"
+  },
+  {
+    "revision": "78b22ba8d6cf47031e024593dfffd3ab",
+    "url": "/pizza-me/static/media/pizza_Pepperoni_Lovers.78b22ba8.webp"
+  },
+  {
+    "revision": "e928df88026e9a26642a87df7c12c938",
+    "url": "/pizza-me/static/media/pizza_Ultimate_Cheese_Lovers.e928df88.webp"
+  },
+  {
+    "revision": "858090fbdc821b37a4e98bd1b5c2cdee",
+    "url": "/pizza-me/static/media/pizza_Veggie_Lovers.858090fb.webp"
+  },
+  {
+    "revision": "f7ed334d0774814ef00ce377e94f416e",
+    "url": "/pizza-me/static/media/pizza_cheese.f7ed334d.jpg"
+  },
+  {
+    "revision": "9f0ca6206f98165dcc7832dc44902905",
+    "url": "/pizza-me/static/media/pizza_meat_lovers.9f0ca620.webp"
+  },
+  {
+    "revision": "50a5d0e84b3b064a2e9d7167640c8b2a",
+    "url": "/pizza-me/static/media/pizza_pepperoni.50a5d0e8.jpg"
+  },
+  {
+    "revision": "df38d9fcd8e6a9bfd239d5bfd945e84f",
+    "url": "/pizza-me/static/media/pizza_supreme.df38d9fc.webp"
+  },
+  {
+    "revision": "847495ad852ba1f06a383c5e2fd0d333",
+    "url": "/pizza-me/static/media/topping_Bacon_ML_extra.847495ad.webp"
+  },
+  {
+    "revision": "494846e3b077d6c3f5ec82cbac31442d",
+    "url": "/pizza-me/static/media/topping_Bacon_ML_reg.494846e3.webp"
+  },
+  {
+    "revision": "f22472f131ef8965f3fa1e4e03fb8f28",
+    "url": "/pizza-me/static/media/topping_BananaPepper_ML_extra.f22472f1.png"
+  },
+  {
+    "revision": "f86003d4a148add861be95d5861274a0",
+    "url": "/pizza-me/static/media/topping_BananaPepper_ML_reg.f86003d4.png"
+  },
+  {
+    "revision": "a8e99f04b152eb6f804dc41fb26910e5",
+    "url": "/pizza-me/static/media/topping_Beef_ML_extra.a8e99f04.webp"
+  },
+  {
+    "revision": "e2851a4b61bb790ea380551580552dd8",
+    "url": "/pizza-me/static/media/topping_Beef_ML_reg.e2851a4b.webp"
+  },
+  {
+    "revision": "43ac224f204c130149e8aada0c951b68",
+    "url": "/pizza-me/static/media/topping_Chicken_ML_extra.43ac224f.webp"
+  },
+  {
+    "revision": "fa9fafd1fa45e52f310acfefee4d000f",
+    "url": "/pizza-me/static/media/topping_Chicken_ML_reg.fa9fafd1.webp"
+  },
+  {
+    "revision": "5b32faf31d5b9c221d121be05c5cf857",
+    "url": "/pizza-me/static/media/topping_GreenBellPepper_ML_reg.5b32faf3.webp"
+  },
+  {
+    "revision": "7b7a38bdeb6962068fe58e316e481e86",
+    "url": "/pizza-me/static/media/topping_Ham_ML_extra.7b7a38bd.webp"
+  },
+  {
+    "revision": "db00ee97bff1e01455177984c3c07e3f",
+    "url": "/pizza-me/static/media/topping_Ham_ML_reg.db00ee97.webp"
+  },
+  {
+    "revision": "0cfee8afb34d546f6e585e4bacd6f555",
+    "url": "/pizza-me/static/media/topping_ItalianSausage_ML_extra.0cfee8af.webp"
+  },
+  {
+    "revision": "61fea832970f143706cb1350734efdd6",
+    "url": "/pizza-me/static/media/topping_ItalianSausage_ML_reg.61fea832.webp"
+  },
+  {
+    "revision": "321b4509142aa4cb43676560cac72731",
+    "url": "/pizza-me/static/media/topping_Jalepeno_ML_extra.321b4509.webp"
+  },
+  {
+    "revision": "60666cfaee149b5804d6c653b4c48f99",
+    "url": "/pizza-me/static/media/topping_Jalepeno_ML_reg.60666cfa.webp"
+  },
+  {
+    "revision": "28389dec24b1bd4a8eeaa6d9ccc1905f",
+    "url": "/pizza-me/static/media/topping_Mushroom_ML_extra.28389dec.webp"
+  },
+  {
+    "revision": "f845b2473b363e69f48c45fa7a6017cf",
+    "url": "/pizza-me/static/media/topping_Mushroom_ML_reg.f845b247.webp"
+  },
+  {
+    "revision": "f467dbde95e91cf8a5374475defec5fd",
+    "url": "/pizza-me/static/media/topping_Olives_black_ML_extra.f467dbde.webp"
+  },
+  {
+    "revision": "388de32c1f7c6dcfcbb6ecccde61dc14",
+    "url": "/pizza-me/static/media/topping_Olives_black_ML_reg.388de32c.webp"
+  },
+  {
+    "revision": "a0e7bd25ad75968b5f37ce5e09ff18c0",
+    "url": "/pizza-me/static/media/topping_Onion_red_ML_extra.a0e7bd25.png"
+  },
+  {
+    "revision": "cff4d4a69519cbabf104166012546260",
+    "url": "/pizza-me/static/media/topping_Onion_red_ML_reg.cff4d4a6.webp"
+  },
+  {
+    "revision": "00591f8d5a53bc0ffaf36b3dc86a0b09",
+    "url": "/pizza-me/static/media/topping_Pepperoni_ML_extra.00591f8d.webp"
+  },
+  {
+    "revision": "0474aa1714a7844d4ae6af551db90e4c",
+    "url": "/pizza-me/static/media/topping_Pepperoni_ML_reg.0474aa17.webp"
+  },
+  {
+    "revision": "376f96ad3ca33ff7d1af94955162fa63",
+    "url": "/pizza-me/static/media/topping_Pineapple_ML_extra.376f96ad.webp"
+  },
+  {
+    "revision": "88b2446667b7934310206e7f07708505",
+    "url": "/pizza-me/static/media/topping_Pineapple_ML_reg.88b24466.webp"
+  },
+  {
+    "revision": "ef9d58c03743f5c6c631d74aba5a9394",
+    "url": "/pizza-me/static/media/topping_Pork_ML_extra.ef9d58c0.webp"
+  },
+  {
+    "revision": "015abf463c51df8f7689e556b09eabda",
+    "url": "/pizza-me/static/media/topping_Pork_ML_reg.015abf46.webp"
+  },
+  {
+    "revision": "c6d181bd883ca236aa2ac52714a3f28b",
+    "url": "/pizza-me/static/media/topping_Spinach_Fresh_ML_extra.c6d181bd.webp"
+  },
+  {
+    "revision": "9a0b03118cde6d9b50ee3ae269efc993",
+    "url": "/pizza-me/static/media/topping_Spinach_Fresh_ML_reg.9a0b0311.webp"
+  },
+  {
+    "revision": "8efd4042023f783d9f122fb346c094d8",
+    "url": "/pizza-me/static/media/topping_Tomato_ML_extra.8efd4042.png"
+  },
+  {
+    "revision": "6d5ebb3817e68a9f880eb0c27005a587",
+    "url": "/pizza-me/static/media/topping_Tomato_ML_reg.6d5ebb38.png"
+  }
+]);
