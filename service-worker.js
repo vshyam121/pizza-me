45917dc2f195f24701d8d4230664d11ca519ebdc
Shya <shya@Shyams-MacBook-Pro.local>
2020-06-18 16:40:55 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/pizza-me/precache-manifest.4ec6e91ac683d1725332c325a361e2fa.js"
+  "/pizza-me/precache-manifest.7a73479690dd98133464485bfddbf91d.js"
 );
 
 self.addEventListener('message', (event) => {
